c6204ab419fd8fc09a8a8ba176f6531d36e258a5
Guilliam Oberholster <guilliamoberholster@gmail.com>
2022-05-25 11:11:58 +1000

    Just showing how we can use lists

--- Program.cs
+++ Program.cs
@@ -13,6 +13,26 @@ namespace Lists
 
             foreach (var number in numbers)
                 Console.WriteLine(number);
+
+            Console.WriteLine();
+            Console.WriteLine("Index of 1:" + numbers.IndexOf(1));
+            Console.WriteLine("Last index of 1:" + numbers.LastIndexOf(1));
+
+            Console.WriteLine("Count" + numbers.Count);
+
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                    numbers.Remove(numbers[i]);
+            }
+           
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+            numbers.Clear();
+            Console.WriteLine("Count" + numbers.Count);
+
+
         }
     }
 }

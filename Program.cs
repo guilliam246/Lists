021c2e6410e967e4b2d01174a497db6e43514199
Guilliam Oberholster <guilliamoberholster@gmail.com>
2022-05-25 10:57:13 +1000

    Showing how to add and add a range to lists

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lists
 {
@@ -6,7 +7,12 @@ namespace Lists
     {
         static void Main(string[] args)
         {
-            
+            var numbers  = new List <int>() { 1, 2, 3, 4 };
+            numbers.Add (1);
+            numbers.AddRange (new int[3] {5 , 6, 7});
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
         }
     }
 }
